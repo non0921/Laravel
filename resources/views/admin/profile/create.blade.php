ae3c5a2e809904075ef37c3653595bc76b36f994
”sasakinozomu” <kirin161@gmail.com>
2019-11-08 18:21:26 +0000

    10laravel

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width, initial-scale=1">
+    
+    <title>Profile</title>
+  </head>
+  <body>
+    <h2>Profile作成画面</h2>
+  </body>
+    
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width, initial-scale=1">
+    
+    <title>Edit</title>
+  </head>
+  <body>
+    <h3>Edit画面</h3>
+  </body>
+    
+</html>
