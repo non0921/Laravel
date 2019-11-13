da9ca71f990243b59865e1c3a175872d8de8b461
”sasakinozomu” <kirin161@gmail.com>
2019-11-13 10:15:51 +0000

    11kadai

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <title>Profile</title>
-  </head>
-  <body>
-    <h2>Profile作成画面</h2>
-    
 {{-- layouts/admin.blade.phpを読み込む --}}
 @extends('layouts.admin')
 
@@ -22,11 +10,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィール新規作成</h2>
+                <h2>Myプロフィール</h2>
             </div>
         </div>
     </div>
 @endsection    
-  </body>
-    
-</html>
+ 
--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,12 @@ Route::get('/', function () {
 });
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-});
-Route::get('xxx', 'Admin\AAAController@bbb');
+    Route::get('xxx', 'Admin\AAAController@bbb');
 
 Route::get('profile/create', 'Admin\ProfileController@add');
 Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+
 
 
 Auth::routes();
