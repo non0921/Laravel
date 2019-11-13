5886a26178039495e416d6bffac1e0822cac2625
”sasakinozomu” <kirin161@gmail.com>
2019-11-13 06:34:35 +0000

    laravel

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-  Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add');
 });
-Route::get('admin/profile/create', 'Admin\ProfileController@add');
-Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+Route::get('xxx', 'Admin\AAAController@bbb');
+
+Route::get('profile/create', 'Admin\ProfileController@add');
+Route::get('profile/edit', 'Admin\ProfileController@edit');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
