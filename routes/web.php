cf10d3de19f598495fbe33e3f42216423c959e34
”sasakinozomu” <kirin161@gmail.com>
2019-11-14 09:51:53 +0000

    11laravel

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('xxx', 'Admin\AAAController@bbb');
 
 Route::get('profile/create', 'Admin\ProfileController@add');
@@ -23,6 +23,10 @@ Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
