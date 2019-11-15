9c8c74e11b1f87e45702f1b77f414e2a895dcc0d
”sasakinozomu” <kirin161@gmail.com>
2019-11-15 16:12:47 +0000

    12kadai

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <title>Edit</title>
-  </head>
-  <body>
-    <h3>Edit画面</h3>
-  </body>
-    
-</html>
+@extends('layouts.profile')
+
+
+@section('title', 'Editの新規作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Edit</h2>
+            </div>
+        </div>
+    </div>
+@endsection    
+ 
