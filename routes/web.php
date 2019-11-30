71177408009dc294b1a9e8ccf7e3fc324b81b7c2
”sasakinozomu” <kirin161@gmail.com>
2019-11-30 14:19:19 +0000

    16Laravel

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::post('news/create', 'Admin\NewsController@create');
-
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); 
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); 
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 Route::get('profile/create', 'Admin\ProfileController@add');
 Route::post('profile/create', 'Admin\ProfileController@create');
 Route::get('profile/edit', 'Admin\ProfileController@edit');
 Route::post('profile/edit', 'Admin\ProfileController@update');
 });
+
 
 
 
