4db92be36a18c758ba132d03fa37b540acde79d6
”sasakinozomu” <kirin161@gmail.com>
2019-12-02 16:39:21 +0000

    17Laravel

--- app/Profile.php
+++ app/Profile.php
@@ -14,4 +14,9 @@ class Profile extends Model
         'hobby' => 'required',
         'introduction' => 'required'
         );
+        
+        public function histories()
+        {
+            return $this->hasmany('App\ProfileHistory');
+        }
 }
