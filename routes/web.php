83f4a3dacd017c2ce2e089e1197fba7ea32ae9fd
”sasakinozomu” <kirin161@gmail.com>
2019-12-06 15:18:13 +0000

    gimon

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,14 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); 
     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); 
     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
-    
+
 Route::get('profile/create', 'Admin\ProfileController@add');
 Route::post('profile/create', 'Admin\ProfileController@create');
 Route::get('profile/edit', 'Admin\ProfileController@edit');
 Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
-
+Route::get('/', 'NewsController@index');
 
 
 Auth::routes();
